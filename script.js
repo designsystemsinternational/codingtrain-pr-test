3ec2a1285c029aae919bfb7fe6526bdb23151ba7
Rune Madsen <rune@runemadsen.com>
2022-06-16 09:27:08 +0200

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+// Make a new branch
+//https://gist.github.com/potherca/3964930
+
+// Add the JSON file
+// Add the image
+//https://docs.github.com/en/rest/repos/contents#create-or-update-file-contents
+
+// Make a PR to main
+// https://www.pluralsight.com/guides/open-a-pull-request-via-the-github-api
